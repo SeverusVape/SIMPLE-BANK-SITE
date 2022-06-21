f7c15e1c648b68b438cef26ab5cab2007292f3eb
Sergei Koshelev <koshelevworkmail@gmail.com>
2022-06-21 19:47:31 -0400

    Intersection observer

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+
 // MODAL WINDOWS
 const openModal = function (e) {
     e.preventDefault();
@@ -24,7 +31,14 @@ btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
-// SMOOTH SCROLLING FOR BTN IN HEADER
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
+
+// *SMOOTH SCROLLING FOR BTN IN HEADER
+
 btnScrollTo.addEventListener("click", function (e) {
     // scrolling
 
@@ -38,9 +52,93 @@ btnScrollTo.addEventListener("click", function (e) {
     section1.scrollIntoView({ behavior: "smooth" });
 });
 
-//
-document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-        closeModal();
+// *PAGE NAVIGATION
+
+// using bubble effect
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+    e.preventDefault();
+    if (e.target.classList.contains("nav__link")) {
+        // get href of element
+        const id = e.target.getAttribute("href");
+        //smooth scrolling
+        document.querySelector(id).scrollIntoView({ behavior: "smooth" });
     }
+});
+
+// *TABBED COMPONENTS
+
+tabsContainer.addEventListener("click", function (e) {
+    const clicked = e.target.closest(".operations__tab");
+
+    // guard clause
+    if (!clicked) return;
+
+    // Remove active classes
+    tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+    tabsContent.forEach((c) =>
+        c.classList.remove("operations__content--active")
+    );
+    // Active tab
+    clicked.classList.add("operations__tab--active");
+
+    // Content area
+    document
+        .querySelector(`.operations__content--${clicked.dataset.tab}`)
+        .classList.add("operations__content--active");
+});
+
+// *MENU FADE ANIMATION
+
+const hndlrHover = function (e) {
+    if (e.target.classList.contains("nav__link")) {
+        const link = e.target;
+        const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+        const logo = link.closest(".nav").querySelector("img");
+
+        siblings.forEach((el) => {
+            if (el !== link) el.style.opacity = this;
+        });
+        logo.style.opacity = this;
+    }
+};
+// Passing argument into handler
+nav.addEventListener("mouseover", hndlrHover.bind(0.5));
+nav.addEventListener("mouseout", hndlrHover.bind(1));
+
+// *STICKY NAVIGATION (OBSERVER API)
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) nav.classList.add("sticky");
+    else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+// *LAZY LOADING SECTIONS (OBSERVER API)
+
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove("section--hidden");
+    observer.unobserve(entry.target);
+};
+
+const sectionsObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+});
+allSections.forEach(function (section) {
+    sectionsObserver.observe(section);
+    section.classList.add("section--hidden");
 });
