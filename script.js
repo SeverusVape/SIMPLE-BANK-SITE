bd0dbc4c9e8826f97efc7d9754ea771d4162ed89
Sergei Koshelev <koshelevworkmail@gmail.com>
2022-06-20 20:36:26 -0400

    Implement smooth scrolling

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 "use strict";
 
-// Modal window
-
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
-const openModal = function () {
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+// MODAL WINDOWS
+const openModal = function (e) {
+    e.preventDefault();
     modal.classList.remove("hidden");
     overlay.classList.remove("hidden");
 };
@@ -17,12 +20,25 @@ const closeModal = function () {
     overlay.classList.add("hidden");
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener("click", openModal);
-
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
+// SMOOTH SCROLLING FOR BTN IN HEADER
+btnScrollTo.addEventListener("click", function (e) {
+    // scrolling
+
+    // *old version (all browsers)
+    /* window.scrollTo({
+        left: s1coords.left + window.pageXOffset,
+        top: s1coords.top + window.pageYOffset,
+        behavior: "smooth",
+    }); */
+    // *new version (modern browsers)
+    section1.scrollIntoView({ behavior: "smooth" });
+});
+
+//
 document.addEventListener("keydown", function (e) {
     if (e.key === "Escape" && !modal.classList.contains("hidden")) {
         closeModal();
